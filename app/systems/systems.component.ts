01e6253c11a26eebfe429a1c409ab4124ecf565a
doralteres <doralteres@gmail.com>
2017-07-25 16:46:12 +0300

    anothe one

--- app/systems/systems.component.ts
+++ app/systems/systems.component.ts
@@ -3,14 +3,7 @@ import { ISystem } from "./System";
 import { SystemService } from "./systems.service";
 
 @Component({
-<<<<<<< HEAD
     templateUrl: 'app/systems/systems.component.html'
-=======
-    selector : 'li-sys',
-    templateUrl: 'app/systems/systems.component.html',
-    providers: [SystemService],
-    
->>>>>>> ef3e151bef8b32311ef1556b23a634362d08c761
 })
 export class SystemsComponent {
     public title : string  = 'Systems';
