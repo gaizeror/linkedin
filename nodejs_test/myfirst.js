8e0c950d8000727f90edcbce8418a5b50749c755
gaizeror <gaizeror@gmail.com>
2017-07-26 05:09:39 +0300

    v0.0.1

--- nodejs_test/myfirst.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var developers = require('./developers-module.js');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(developers.getAllDevelopers);
-}).listen(8080);
